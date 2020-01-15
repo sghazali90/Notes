c6955dbefbfc9fd9c4568bfe090525413e440562
sghazali90 <59652139+sghazali90@users.noreply.github.com>
2020-01-15 08:15:25 -0500

    Create Hostess.java

---
+++ restaurant/Hostess.java
@@ -0,0 +1,11 @@
+public class Hostess2 {
+
+	// method for greeting people
+	public static void greet(int numOfPeople) {
+		System.out.println("Hi, and welcome to Friday's!");
+		System.out.println("I see there are " + numOfPeople
+				+ " people having dinner tonight");
+		System.out.println("Please follow me.");
+
+	}
+}
