a448f5077a3432f46464a2809ee8439e4fd707e3
sghazali90 <59652139+sghazali90@users.noreply.github.com>
2020-01-15 08:22:54 -0500

    Update restaurant.java

--- restaurant/restaurant.java
+++ restaurant/restaurant.java
@@ -1,31 +1,40 @@
-
 public class Restaurant2 {
-	
-	//Create a method to print an array to screen
-	public static void printArray(String array[]){
+
+	// Create a method to print an array to screen
+	public static void printArray(String array[]) {
 		for (int i = 0; i < array.length; i++) {
-			 if (i > 0) {
-				 System.out.print(", ");
-		         }
-			 	System.out.print(array[i]);
-		      }
+			if (i > 0) {
+				System.out.print(", ");
+			}
+			System.out.print(array[i]);
+		}
 	}
-	
-	
+
 	public static void main(String[] args) {
 
-		//Number of people to be seated
-		int party = 5; 
-		
-		//Call the greet method in the Hostess class
+		// Number of people to be seated
+		int party = 5;
+
+		// Call the greet method in the Hostess class
 		Hostess2.greet(party);
-		
-		//Call the  getDrinks method in the Server class
-		String[ ] drinks;
+
+		// Drinks Order
+		String[] drinks;
 		drinks = Waitstaff.getDrinks(party);
-		
-		//Print the array of drinks
+
+		System.out.println("Here are the drinks you ordered: ");
+		// Print the array of drinks
 		printArray(drinks);
+
+		//Food Order
+		String[] food;
+		food= Waitstaff.getFood(party);
+		
+		System.out.println("Here is what you ordered: ");
+		// Print the array of food orders
+		printArray(food);
+		
+		
 	}
 
 }
